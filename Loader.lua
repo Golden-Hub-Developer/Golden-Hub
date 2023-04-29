5411c68cd3db42d145a99c399214a213bb0aacbd
Golden-Hub-Developer <129693098+Golden-Hub-Developer@users.noreply.github.com>
2023-04-29 18:34:06 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,6 @@
 local Games = {
    [4566572536] = "https://raw.githubusercontent.com/Golden-Hub-Developer/Golden-Hub/main/Vehicle%20Legends.lua", -- Vehicle Legends
+   [4410049285] = "https://raw.githubusercontent.com/Golden-Hub-Developer/Golden-Hub/main/Driving%20Simulator.lua", -- Vehicle Legends
 }
 
 if Games[game.PlaceId] then
